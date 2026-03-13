76695f3bca0baacc2252c5d3ec9b25c348676d22
Harris <hong55224411@gmail.com>
2026-03-13 18:12:32 +0800

    NEW

--- YOLO26test.py
+++ YOLO26test.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-model = YOLO('yolo26n.pt')
-model.predict(source='WhatsApp Image 2026-02-03 at 10.36.32 (1).jpeg', conf=0.25, save=True, show=True)
+model = YOLO('best (1).pt')
+model.predict(source='0. Star Photo', conf=0.25, save=True, show=True)
